e80d5d0366b6753cd17e75d5fa682df81a1386d4
Luiz Felipe <luizfeliperamoszero@gmail.com>
2025-11-25 17:59:14 -0300

    commit 4 - move and rename amountFor to getCharge in Rental

--- src/Customer.java
+++ src/Customer.java
@@ -28,7 +28,7 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            double thisAmount = amountFor(each);
+            double thisAmount = each.getCharge();
 
             frequentRenterPoints++;
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
@@ -44,25 +44,4 @@ public class Customer {
                 " frequent renter points";
         return result;
     }
-
-    private double amountFor(Rental aRental) {
-        double thisAmount = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2)
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
-
 }
--- src/Rental.java
+++ src/Rental.java
@@ -1,5 +1,4 @@
 public class Rental {
-
     private Movie _movie;
     private int _daysRented;
 
@@ -14,5 +13,25 @@ public class Rental {
 
     public Movie getMovie() {
         return _movie;
+    }
+
+    public double getCharge() {
+        double result = 0;
+        switch (_movie.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (_daysRented > 2)
+                    result += (_daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += _daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (_daysRented > 3)
+                    result += (_daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
     }
 }
